08e674658e8f0a713a1c046558b147a712b50dc2
Wteia <wteia@wteia.com>
2018-08-08 20:39:25 -0300

    pagina home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,86 +1,264 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Home</title>	
-		<script src="https://ajax.googleapis.com/ajax/libs/
-		jquery/3.1.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://
-		maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/
-		bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/
-		bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<style type="text/css">
-			.box{
-				width: 600px;
-				margin:0 auto;
-				border:1px solid #ccc;			
-			}
-		</style>
-	</head>
-	<body>
-		<br />
-		<div class="container box">
-			<h3 align="center"> Home</h3>
-			<br/>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Home - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    
+   <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ URL::asset ('css/landing-page.min.css') }}" rel="stylesheet">
+	 <link href="{{ URL::asset ('vendor/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
+	 <link href="{{ URL::asset ('vendor/bootstrap/js/bootstrap.js') }}" rel="stylesheet">
+<!-- Custom styles for this template -->
+    <style type="text/css">
+body {
+  padding-top: 54px;
+  background-image: url('img/bg-masthead.jpg');
+  background-size: cover;
+}
+
+@media (min-width: 992px) {
+  body {
+    padding-top: 86px;
+  }
+}
+</style>
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Feederation</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Perfil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Amigos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
 			
-			@if(isset(Auth::user()->email))
-				<?php
-					$persona_id = Auth::user()->personaPadrao;
-					$usuario_id = Auth::user()->id;
-				?>
-				<div class="alert alert-danger success-block">
-					<strong>Bem-vindo {{Auth::user()->nome}} logado como {{Auth::user()->nome_persona }} </strong>
-					<br/>
-					<form method="get" action="{{url('editarUsuario')}}{{$usuario_id}}">
-						<input type="submit" name="editarUsuario" class="btn btn-primary" value="Editar Usuario" /> 						
-					</form>	
-					<form method="get" action="{{url('escolherPersona')}}">
-						<input type="submit" name="editarUsuario" class="btn btn-primary" value="Escolher Persona" /> 						
-					</form>	
-					<form method="post" action="{{url('minhasGuildas')}}">
-						{{ csrf_field() }}
-						<input type="submit" name="minhasGuildas" class="btn btn-primary" value="Minhas Guildas" /> 						
-					</form>			
-					<form method="post" action="{{url('criarGuilda')}}">
-						{{ csrf_field() }}
-						<input type="hidden" name="id" value="<?php echo $persona_id;?>" />
-						<input type="submit" name="criarGuilda" class="btn btn-primary" value="Criar Guild" />				
-					</form>
-					<form method="post" action="{{url('criarPersona')}}">
-						{{ csrf_field() }}
-						<input type="hidden" name="id" value="<?php echo $usuario_id;?>" />
-						<input type="submit" name="criarPersona" class="btn btn-primary" value="Criar Persona" />				
-					</form>
-					<form method="post" action="{{url('Novo Post')}}">
-						{{ csrf_field() }}
-						<input type="hidden" name="id" value="<?php echo $usuario_id;?>" />
-						<input type="submit" name="criarPersona" class="btn btn-primary" value="Postar" />				
-					</form>
-					<div>
-					<tr><td>Posts</td></tr>
-					@foreach ($posts as $post)
-					{{ csrf_field() }}
-					<tr>
-						<td>{{ $post->nome_persona }} </td>
-						<td>
-							{{ $post->conteudo }}
-						</td>
-					</tr>				
-	
-					@endforeach
-					</div>
-					
-					
-					
-					<a href="{{ url('/main/sair')}}">Sair</a>
-				</div>
-			@else
-				<script>
-					window.location="/main";
-				</script>
-			@endif
 			
-			<br/>
-		</div>
-	</body>
+			<div class="card my-4">
+            <h5 class="card-header">Criar publicação</h5>
+            <div class="card-body">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Publique aqui =)">
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Publicar</button>
+                </span>
+              </div>
+            </div>
+          </div>
+
+
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+          <!-- Blog Post -->
+          <div class="card mb-4">
+            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title">Post Title</h2>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+              <a href="#" class="btn btn-primary">Read More &rarr;</a>
+            </div>
+            <div class="card-footer text-muted">
+              Posted on January 1, 2017 by
+              <a href="#">Start Bootstrap</a>
+            </div>
+          </div>
+
+         
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+	       
+        <div class="col-md-4">
+
+          <!-- Search Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+			<div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Side Widget</h5>
+            <div class="card-body">
+              </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+
+
+
+    <!-- Footer-->
+    <footer class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+            <ul class="list-inline mb-2">
+              <li class="list-inline-item">
+                <a href="#">About</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Contact</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Terms of Use</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+            <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2018. All Rights Reserved.</p>
+          </div>
+          <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item mr-3">
+                <a href="#">
+                  <i class="fa fa-facebook fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mr-3">
+                <a href="#">
+                  <i class="fa fa-twitter fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-instagram fa-2x fa-fw"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
 </html>

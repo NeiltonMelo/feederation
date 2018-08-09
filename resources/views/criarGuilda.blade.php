ccaebca2a0505b8fb8a054f2101d711278c27fb2
Wteia <wteia@wteia.com>
2018-08-09 01:35:32 -0300

    tela criar guilda completa

--- resources/views/criarGuilda.blade.php
+++ resources/views/criarGuilda.blade.php
@@ -1,61 +1,205 @@
-
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-	<head>
-		<title>Feederation - Criar Guilda</title>	
-		<script src="https://ajax.googleapis.com/ajax/libs/
-		jquery/3.1.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://
-		maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/
-		bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/
-		bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<style type="text/css">
-			.box{
-				width: 600px;
-				margin:0 auto;
-				border:1px solid #ccc;			
-			}
-		</style>
-	</head>
-	<body>
-		<br />
-		<div class="container box">
-			<h3 align="center"> Criar Guilda</h3>
-			<br/>
-				
-			
-			@if(count($errors) >0)
-			
-				<div class="alert alert-danger">
-					<ul>
-						@foreach($errors->all() as $error)
-							<li>{{$error}}</li>
-						@endforeach
-					</ul>
-				</div>
-			@endif
-			<?php
-					$id = Auth::user()->personaPadrao;
-				?>
-			<form method="post" action="inserirGuilda">
-			   {{ csrf_field() }}
-				
-				<input type="hidden" name="administrador_id" value="<?php echo $id;?>" />
-				
-				<div class="form-group">
-					<label>Nome</label>
-					<input type="text" name="nome" class="form-control" required value="" />
-				</div>
-				<div class="form-group">
-					<label>Game</label>
-					<input type="text" name="game_nome" class="form-control" required value="" />
-				</div>	
-				<div class="form-group">
-					<input type="submit" name="criar" class="btn btn-primary" value="Criar" />
-				</div>
-			</form>
-		</div>
-		
-	</body>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Feederation - Cadastro de Guilda</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/landing-page.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+<style type="text/css">
+body#LoginForm{ background-image:url("img/bg-masthead.jpg"); background-repeat:no-repeat; background-position:center; background-size:cover; padding:0px;}
+.form-heading { color:#fff; font-size:23px;}
+.panel h2{ color:#444444; font-size:18px; margin:0 0 8px 0;}
+.panel p { color:#777777; font-size:14px; margin-bottom:30px; line-height:24px;}
+
+
+.login-form .form-control {
+  border: 1px solid #d4d4d4;
+  border-radius: 4px;
+  font-size: 14px;
+  height: 50px;
+  line-height: 50px;
+}
+.main-div {
+  background: #ffffff none repeat scroll 0 0;
+  border-radius: 2px;
+  margin: 10px auto 30px;
+  max-width: 38%;
+  padding: 50px 70px 70px 71px;
+}
+
+.login-form .form-group {
+  
+  margin-bottom:10px;
+}
+.login-form{ 
+margin-top:70px;
+text-align:center;}
+.forgot a {
+  color: #777777;
+  font-size: 14px;
+  text-decoration: underline;
+}
+.login-form  .btn.btn-primary {
+  background: #f0ad4e none repeat scroll 0 0;
+  border-color: #f0ad4e;
+  color: #ffffff;
+  font-size: 14px;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  padding: 0;
+}
+.forgot {
+  text-align: left; margin-bottom:30px;
+}
+.botto-text {
+  color: #ffffff;
+  font-size: 14px;
+  margin: auto;
+}
+.login-form .btn.btn-primary.reset {
+  background: #ff9900 none repeat scroll 0 0;
+}
+.back { text-align: left; margin-top:10px;}
+.back a {color: #444444; font-size: 13px;text-decoration: none;}
+
+</style>
+
+  </head>
+
+  <body id="LoginForm">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Feederation</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/home">Perfil</a>
+            </li>
+				      
+            <li class="nav-item">
+              <a class="nav-link" href="{{url('escolherPersona')}}">Personas</a>
+				</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Amigos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ url('/main/sair')}}">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+
+<div class="container">
+<h1 class="form-heading"></h1>
+<div class="login-form">
+<div class="main-div">
+    <div class="panel">
+   <h2>Cadastro de Guilda</h2>
+   <p>Preencha os campos abaixo</p>
+   </div>
+    <form id="Login">
+			<div>			
+				<img src="img/guild_image.jpeg" class="img-fluid" alt="Responsive image">
+   		</div>     
+        <br>
+        <div class="form-group">
+
+            <input type="email" class="form-control" id="nome" placeholder="Nome da guilda">
+
+        </div>
+
+        
+        <button type="submit" class="btn btn-primary">Cadastrar guilda</button>
+
+    </form>
+    </div>
+    
+    
+</div></div></div>
+
+    <!-- Footer -->
+    <footer class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+            <ul class="list-inline mb-2">
+              <li class="list-inline-item">
+                <a href="#">About</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Contact</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Terms of Use</a>
+              </li>
+              <li class="list-inline-item">&sdot;</li>
+              <li class="list-inline-item">
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+            <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2018. All Rights Reserved.</p>
+          </div>
+          <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item mr-3">
+                <a href="#">
+                  <i class="fa fa-facebook fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mr-3">
+                <a href="#">
+                  <i class="fa fa-twitter fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-instagram fa-2x fa-fw"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
 </html>
